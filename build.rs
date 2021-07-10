72462d60f3a4e0e368ba74ec71c79c045c498881
Gene Erik <geneerik@fossdevops.com>
2021-07-10 01:31:49 -0700

    8sdfg

--- build.rs
+++ build.rs
@@ -17,11 +17,12 @@ use structopt::clap::Shell;
 use structopt::StructOpt;
 
 // neat trick from https://github.com/mmstick/cargo-deb/blob/e43018a46b8dc922cfdf6cdde12f7ed92fcc41aa/example/build.rs
+// to get around poisoning the source dirt during packaging
 use std::path::PathBuf;
 use std::fs;
-//use std::io::Write;
 
 fn main() {
+    // get the out_dir consistent base path (the target dir) and append assets directory
     let out_str = env::var("OUT_DIR").unwrap();
     let outdir = PathBuf::from(&out_str);
     let mut outdir = outdir
@@ -30,19 +31,13 @@ fn main() {
         .next().unwrap().to_owned();
         outdir.push("assets");
 
+    // create the assets directory if it does not already exist
     if !outdir.exists() {
         fs::create_dir(&outdir).expect("Could not create assets dir");
     }
-    //File::create(out_path.join("5.txt")).and_then(|mut f| f.write_all(b"Hello generated asset 1")).expect("Could not write asset file");
-    //File::create(out_path.join("6.txt")).and_then(|mut f| f.write_all(b"Hello generated asset 2")).expect("Could not write asset file");
 
     // generate completion scripts, zsh does panic for some reason
     for shell in Shell::variants().iter().filter(|shell| **shell != "zsh") {
-        //let outdir = match env::var_os("OUT_DIR") {
-        //    None => return,
-        //    Some(outdir) => outdir,
-        //};
-
         Args::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::from_str(shell).unwrap(), &outdir);
     }
 }
