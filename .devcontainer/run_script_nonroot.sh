1afff1c4743c8c908aa07c97d3f08026ba139af9
Gene Erik <geneerik@fossdevops.com>
2021-07-10 21:21:04 -0700

    Added code to github action script to update the user in the container

--- .devcontainer/run_script_nonroot.sh
+++ .devcontainer/run_script_nonroot.sh
@@ -10,12 +10,15 @@ if [[ "${PARAMS}" == '' ]]; then
 fi
 set -euo pipefail
 PARAMS=("$@")
-#if [[ "${#PARAMS[@]}" -lt 2 ]]; then
-#  echo 'Script requires atleast 2 parameters'; echo "$0 IMAGE_PATH DEVICE_PATH"; false
-#fi
+if [[ "${#PARAMS[@]}" -gt 1 ]]; then
+  NEW_UID="${PARAMS[1]}"
+fi
+if [[ "${#PARAMS[@]}" -gt 2 ]]; then
+  NEW_GID="${PARAMS[2]}"
+fi
 
 BASH_SCRIPT_CONTENTS="${PARAMS[0]}"
-BASH_SCRIPT_PATH="${PARAMS[1]:-/tmp/bash.script}"
+BASH_SCRIPT_PATH="${PARAMS[3]:-/tmp/bash.script}"
 
 printf '%s' "#!/bin/bash
 set -e
@@ -28,4 +31,20 @@ set -euo pipefail
 ${BASH_SCRIPT_CONTENTS}" > "${BASH_SCRIPT_PATH}"
 chmod 777 "${BASH_SCRIPT_PATH}"
 username=$(ls /home)
+
+# change the uid for the user if requested
+if [[ -n "${NEW_UID:-}" ]]; then
+  usermod -u "${NEW_UID}" "${username}"
+fi
+# change the gid for the user if requested
+if [[ -n "${NEW_GID:-}" ]]; then
+  groupmod -g "${NEW_GID}" "${username}"
+fi
+# change ownership of the home directory and contents if uid or gid have changed
+if [[ -n "${NEW_UID:-}" || -n "${NEW_GID:-}" ]]; then
+  chown -R "$(id -u ${username}):$(id -g ${username})" "/home/${username}"
+fi
+
+chown "$(id -u ${username}):$(id -g ${username})" "${BASH_SCRIPT_PATH}"
+
 exec su -s /bin/bash -c "${BASH_SCRIPT_PATH}" "${username}"
